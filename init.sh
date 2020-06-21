1fa844ccaa4ec220f683cae346bd478691d6bd4a
calcifer777 <m.filip@hotmail.it>
2020-06-22 01:14:54 +0200

    tmux and init script

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: init.sh
+# 
+#         USAGE: ./init.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Marco Filippone, 
+#  ORGANIZATION: 
+#       CREATED: 06/22/2020 00:46
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo Script path: $SCRIPT_PATH
+
+# Tmux
+TMUX_PATH=~/.tmux.conf
+test -e $TMUX_PATH && mv $TMUX_PATH $TMUX_PATH.backup 
+echo "ln -l ${SCRIPT_PATH}/tmux/.tmux.conf ${TMUX_PATH}"
+ln -s ${SCRIPT_PATH}/tmux/.tmux.conf ${TMUX_PATH}
+
